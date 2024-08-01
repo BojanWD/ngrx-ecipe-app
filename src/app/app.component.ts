32b505e239cec4612d74dfcc75ced53a464dad2e
Predrag Carapic <pero0904@gmail.com>
2024-08-01 16:55:37 +0200

    Create cart component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RecipesListFacadeComponent } from './recipes/recipes-list-facade/recipes-list-facade.component';
 import { IngredientsListFacadeComponent } from './ingredients/ingredients-list-facade/ingredients-list-facade.component';
+import { CartComponent } from './cart/cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RecipesListFacadeComponent, IngredientsListFacadeComponent],
+  imports: [
+    RecipesListFacadeComponent,
+    IngredientsListFacadeComponent,
+    CartComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/cart/cart/cart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.scss'
+})
+export class CartComponent {
+
+}
